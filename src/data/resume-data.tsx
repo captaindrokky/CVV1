9cb776f29cab31570073b899a72d6e5613786546
Mauricio Tovar <78090543+captaindrokky@users.noreply.github.com>
2024-09-30 21:40:10 -0600

    Update resume-data.tsx
    change1.1

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -22,12 +22,10 @@ import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 export const RESUME_DATA = {
   name: "Mauricio Tovar",
   initials: "MT",
-  location: " BJ, CDMX",
+  location: "BJ, CDMX",
   locationLink: "contact@tovarcode.me",
-  about:
-    "Developer Engineer UX IX",
-  summary:
-    "Para obtener un puesto desafiante como Asistente de Asesor, donde mi habilidad para comunicarme eficazmente, mi capacidad para organizar información y mi compromiso con la excelencia en el servicio puedan ser utilizados para brindar un apoyo valioso al equipo y a los clientes .",
+  about: "Developer Engineer UX IX",
+  summary: "Para obtener un puesto desafiante como Asistente de Asesor, donde mi habilidad para comunicarme eficazmente, mi capacidad para organizar información y mi compromiso con la excelencia en el servicio puedan ser utilizados para brindar un apoyo valioso al equipo y a los clientes.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
   personalWebsiteUrl: "",
   contact: {
@@ -53,11 +51,13 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: "Oracle ",
+      school: "Oracle",
       degree: "Developer Front End",
       start: "2023",
       end: "2024",
-      school: "Dev F Bootcamp ",
+    },
+    {
+      school: "Dev F Bootcamp",
       degree: "Developer Front End",
       start: "2022",
       end: "2023",
@@ -65,25 +65,23 @@ export const RESUME_DATA = {
   ],
   work: [
     {
-      company: "VW AUDI CUPRA ",
+      company: "VW AUDI CUPRA",
       link: "",
       badges: ["Remote"],
       title: "Client Care",
       logo: ParabolLogo,
       start: "2021",
       end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+      description: "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
     },
     {
       company: "ATT SOPORTE",
       link: "https://ATT.COM",
-      badges: ["Presencial "],
+      badges: ["Presencial"],
       title: "Soporte técnico",
       start: "2018",
       end: "2019",
-      description:
-        "Soporte tecnico conección a radio bases",
+      description: "Soporte técnico conexión a radio bases",
     },
   ],
   skills: [
@@ -91,5 +89,4 @@ export const RESUME_DATA = {
     "React/Next.js/Remix",
     "Figma",
   ],
-  
 } as const;
